7ecdb28a4c8cd4d7b6f9a0b0d3b7f3effd849853
webgestalter <git@webgestalter.in>
2019-11-08 19:31:08 +0100

    ...

--- JPGandMP3toMP4.sh
+++ JPGandMP3toMP4.sh
@@ -6,7 +6,5 @@ NAME=$( zenity \
   --text="give the name" \
   )
 
-echo $NAME
-
 cd "/media/stvn/Elements/Projekte/We dont die we multiply/Lumpenprolet/Das Kapital/Prozess/";
 ffmpeg -r 1 -loop 1 -i $NAME.jpg -i $NAME.mp3 -acodec copy -r 1 -shortest -vf scale=1920:1080 $NAME.mp4
