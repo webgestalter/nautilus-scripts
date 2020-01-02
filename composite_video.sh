e4cafe173339ab7b99a53ad9a1170bb5cd53ce49
webgestalter <git@webgestalter.in>
2020-01-02 19:48:24 +0100

    ...

--- composite_video.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$#" -ne 2 ]; then
-    echo "Call the script with ./$(basename "$0") (path to image) (path to video)"
-else
-    ffmpeg -r 1 -loop 1 -i "$1" -i "$2" -acodec copy -r 1 -shortest -vf scale=1920:1080 video.mp4
-fi
