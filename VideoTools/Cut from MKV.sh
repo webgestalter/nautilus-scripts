1f5b53de03efc790c4fcbf1d10bbf348aaafdb48
webgestalter <git@webgestalter.in>
2020-02-24 19:58:36 +0100

    ...

--- VideoTools/Cut from MKV.sh
+++ VideoTools/Cut from MKV.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 INPUT="$(basename "${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS}")"
-FROM=$( zenity --entry="" --title="Cut Part from MKV" --text="Startpoint (hh:mm:ss)" )
-  TO=$( zenity --entry="" --title="Cut Part from MKV" --text="Endpoint (hh:mm:ss)" )
-NAME=$( zenity --entry="" --title="Cut Part from MKV" --text="Name of Output File" )
+
+FORM=$( zenity --forms \
+  --title="Cut Part from MP4" \
+  --separator="," \
+  --text="Cut Part from MP4" \
+  --add-entry="Startpoint (hh:mm:ss)" \
+  --add-entry="Endpoint (hh:mm:ss)" \
+  --add-entry="Name of Output File")
+
+FROM=$(awk -F, '{print $1}' <<<$FORM)
+TO=$(awk -F, '{print $2}' <<<$FORM)
+NAME=$(awk -F, '{print $3}' <<<$FORM)
 
 ffmpeg -i "$INPUT" -ss $FROM -to $TO -c:v copy -c:a copy "$NAME".mkv
