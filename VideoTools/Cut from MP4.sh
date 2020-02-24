37f46c252ba015a7de1afbbdeb491d57dc77e7d6
webgestalter <git@webgestalter.in>
2020-02-24 20:07:13 +0100

    ...

--- VideoTools/Cut from MP4.sh
+++ VideoTools/Cut from MP4.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-FROM=$( zenity --entry="" --title="Cut Part from MP4" --text="Startpoint (hh:mm:ss)" )
-  TO=$( zenity --entry="" --title="Cut Part from MP4" --text="Endpoint (hh:mm:ss)" )
-NAME=$( zenity --entry="" --title="Cut Part from MP4" --text="Name of Output File" )
+INPUT="$(basename "${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS}")"
 
-ffmpeg -i "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" -ss $FROM -to $TO -c:v copy -c:a copy "$NAME".mp4
+FORM=$( zenity --forms --title="Cut Part from MP4" --separator="," --text="Cut Part from MP4" \
+  --add-entry="Startpoint (hh:mm:ss)" \
+  --add-entry="Endpoint (hh:mm:ss)" \
+  --add-entry="Name of Output File")
+
+FROM=$(awk -F, '{print $1}' <<<$FORM)
+TO=$(awk -F, '{print $2}' <<<$FORM)
+NAME=$(awk -F, '{print $3}' <<<$FORM)
+
+ffmpeg -i "$INPUT" -ss $FROM -to $TO -c:v copy -c:a copy "$NAME".mp4
--- VideoTools/Cut from VOB.sh
+++ VideoTools/Cut from VOB.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-FROM=$( zenity --entry="" --title="Cut Part from VOB" --text="Startpoint (hh:mm:ss)" )
-  TO=$( zenity --entry="" --title="Cut Part from VOB" --text="Endpoint (hh:mm:ss)" )
-NAME=$( zenity --entry="" --title="Cut Part from VOB" --text="Name of Output File" )
+INPUT="$(basename "${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS}")"
 
-ffmpeg -i "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" -ss $FROM -to $TO -c:v copy -c:a copy "$NAME".vob
+FORM=$( zenity --forms --title="Cut Part from VOB" --separator="," --text="Cut Part from VOB" \
+  --add-entry="Startpoint (hh:mm:ss)" \
+  --add-entry="Endpoint (hh:mm:ss)" \
+  --add-entry="Name of Output File")
+
+FROM=$(awk -F, '{print $1}' <<<$FORM)
+TO=$(awk -F, '{print $2}' <<<$FORM)
+NAME=$(awk -F, '{print $3}' <<<$FORM)
+
+ffmpeg -i "$INPUT" -ss $FROM -to $TO -c:v copy -c:a copy "$NAME".vob
