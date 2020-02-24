89cccdf4dc3ef76ce666b976c17ef3e44762f31e
webgestalter <git@webgestalter.in>
2020-02-24 18:25:31 +0100

    ...

--- VideoTools/Set Aspect Ratio to 239:100.sh
+++ VideoTools/Set Aspect Ratio to 239:100.sh
@@ -2,6 +2,6 @@
 
 
 INPUT="$(basename "${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS}")"
-NAME=$( zenity --entry="" --title="Cut Part from MP4" --text="Name of Output File" )
+NAME=$( zenity --entry="" --title="Set Aspect Ratio" --text="Name of Output File" )
 
 ffmpeg -i "$INPUT" -aspect 239:100 -c copy "$NAME"
